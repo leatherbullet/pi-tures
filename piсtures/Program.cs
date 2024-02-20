7e1eac5824704c1b69824579e9f171ea3f260cde
79612 <79612@DESKTOP-O1P5TC5>
2024-02-20 08:17:38 +0300

    Добавьте файлы проекта.

---
+++ piсtures/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace piсtures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictures = 52;
+            int picturesInRow = 3;
+            int page;
+            int overage;
+
+            page = pictures / picturesInRow;
+            overage = pictures % picturesInRow;
+
+            Console.WriteLine($"Всего рядов: {page}, остаток: {overage}");
+
+        }
+    }
+}
